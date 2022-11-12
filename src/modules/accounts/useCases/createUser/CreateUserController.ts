cb27428e43736fa8c39857f3b771d1301fef97e2
Gabriel Desidério <gabrieldesd7@gmail.com>
2022-11-12 15:27:36 -0300

    create user

---
+++ src/modules/accounts/useCases/createUser/CreateUserController.ts
@@ -0,0 +1,25 @@
+import { Request, Response } from "express";
+import { container } from "tsyringe";
+import { CreateUserUseCase } from "./CreateUserUseCase";
+
+class CreateUserController {
+  async handle(request: Request, response: Response): Promise<Response> {
+    try {
+      const { name, email, password, driver_license } = request.body;
+      const createUserUseCase = container.resolve(CreateUserUseCase);
+
+      await createUserUseCase.execute({
+        name, 
+        email, 
+        password, 
+        driver_license
+      });
+
+      return response.status(201).send();
+    } catch (error) {
+      return response.status(500).json({ message: error.message });
+    }
+  }
+}
+
+export { CreateUserController }
