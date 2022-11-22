547e12e82ff033fe328aba6f1744b13acb087e26
Gabriel Desidério <gabrieldesd7@gmail.com>
2022-11-22 16:52:38 -0300

    use paths to fix imports

--- src/server.ts
+++ src/server.ts
@@ -9,8 +9,8 @@ createConnection();
 import { router } from './routes';
 import swaggerFile from './swagger.json'
 
-import './shared/container';
-import { AppError } from './errors/AppError';
+import '@shared/container';
+import { AppError } from '@errors/AppError';
 
 const app = express();
 
